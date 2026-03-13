561e097e2695a00d1876f8cb1ca647eff4aef917
David Taylor <dktaylor1701@gmail.com>
2026-03-12 23:11:46 -0400

    Initial content view

--- SnackBackExample.swiftpm/ContentView.swift
+++ SnackBackExample.swiftpm/ContentView.swift
@@ -1,37 +1,96 @@
-import SwiftUI
 import SnackBack
 import SnackBackModels
+import SwiftUI
 
 struct ContentView: View {
-    @State
-    var message: String = ""
-    
-    let service: SnackBackServicing
-    
-    init(key: String) {
-        self.service = SnackBackService(key: key)
+  @State
+  var presenting: Bool = false
+
+  var body: some View {
+    Button {
+      presenting = true
+    } label: {
+      Text("Show")
     }
-    
-    var feedback: FeedbackContent {
-        FeedbackContent(message: message)
-    }
-    
-    var body: some View {
-        Form {
-            Section {
-                 
-                TextField(text: $message) {
-                    Text("Let us know what you think")
-                }   
-            }
-            Section {
-                Button {
-//                    service.submit(feedback: FeedbackContent(message: message))
-                } label: {
-                    Text("Submit")
-                }    
-            }
-            
+    .sheet(
+      isPresented: $presenting,
+      content: {
+        Feedback(key: "test")
+      })
+  }
+}
+
+struct Feedback: View {
+  @State
+  private var message: String = ""
+
+  @State
+  private var submitting: Bool = false
+
+  @Environment(\.dismiss)
+  private var dismiss
+
+  private var feedback: FeedbackContent? {
+    guard !message.isEmpty else { return nil }
+
+    return FeedbackContent(message: message)
+  }
+
+  private let service: SnackBackServicing
+
+  public init(key: String) {
+    self.init(service: SnackBackService(key: key))
+  }
+
+  public init(service: SnackBackServicing) {
+    self.service = service
+  }
+
+  @ViewBuilder
+  var form: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField(text: $message) {
+            Text("Let us know what you think")
+          }
         }
+        Section {
+          Button {
+            guard let feedback else { return }
+
+            Task {
+              submitting = true
+              try? await service.submit(feedback: feedback)
+              submitting = false
+              dismiss()
+            }
+          } label: {
+            Text("Submit")
+          }
+          .disabled(feedback == nil)
+        }
+      }
+      .navigationTitle("Submit Feedback")
+      .toolbar {
+        ToolbarItem(placement: .cancellationAction) {
+          Button {
+            dismiss()
+          } label: {
+            Text("Cancel")
+          }
+        }
+      }
     }
+  }
+
+  var body: some View {
+    ZStack {
+      form
+        .disabled(submitting)
+      if submitting {
+        ProgressView()
+      }
+    }
+  }
 }
--- SnackBackExample.swiftpm/MyApp.swift
+++ SnackBackExample.swiftpm/MyApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct MyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView(key: "test")
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
